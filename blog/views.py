985a7c028fbd9056c639f4eb4ac5106cce9c5405
adem kalfalı <ademkalfa9@gmail.com>
2023-01-09 23:14:52 +0300

    ModelViewSet

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Category, Blog
+from .serializers import CategorySerializer, BlogSerializer
+
+class CategoryView(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filterset_fields =['name']
+
+class BlogView(ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    filterset_fields = ['category__name']
+
+
+
+
